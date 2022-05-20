a4b162b1f6499ba668a4fa3d9cb564a32253d803
Alfredo España <alfespa17@gmail.com>
2022-05-19 19:04:00 -0600

    Enable to reply message in Slack notification (#25)
    * Enable to reply message in Slack notification using threads

--- groovy/SlackApp/README.md
+++ groovy/SlackApp/README.md
@@ -2,7 +2,7 @@
 
 [Slack](https://slack.dev/java-slack-sdk/) is an online messaging app.
 
-# Example:
+# Example simple notification:
 ```yaml
 flow:
   - type: "customScripts"
@@ -24,3 +24,44 @@ flow:
 ```
 
 > ***$SLACK_TOKEN*** is an environment variable define at workspace level, ***terrakubeOutput*** is the terrakube job output variable.
+
+# Example nested notification:
+```yaml
+flow:
+  - type: "customScripts"
+    step: 100
+    commands:
+      - runtime: "GROOVY"
+        priority: 100
+        after: true
+        script: |
+          import SlackApp
+
+          SlackApp slackApp = new SlackApp("$workingDirectory")
+          
+          slackApp.sendMessage(
+              "#general", 
+              "Hello Terrakube!", 
+              "$SLACK_TOKEN", 
+              terrakubeOutput);
+          "Slack Message Completed..."
+      - runtime: "GROOVY"
+        priority: 200
+        after: true
+        script: |
+          import SlackApp
+
+          SlackApp slackApp = new SlackApp("$workingDirectory")
+          
+          slackApp.sendMessage(
+              "#general", 
+              "First reply to message!", 
+              "$SLACK_TOKEN", 
+              terrakubeOutput);
+
+          slackApp.sendMessage(
+              "#general", 
+              "First reply to message!", 
+              "$SLACK_TOKEN", 
+              terrakubeOutput);
+```
--- groovy/SlackApp/SlackApp.groovy
+++ groovy/SlackApp/SlackApp.groovy
@@ -7,18 +7,53 @@ import com.slack.api.methods.request.chat.ChatPostMessageRequest
 import com.slack.api.methods.response.chat.ChatPostMessageResponse
 
 class SlackApp {
+
+    String workingDirectory
+
+    SlackApp(workingDirectory){
+        this.workingDirectory = workingDirectory
+    }
+
+    SlackApp(){
+        this.workingDirectory = ""
+    }
+
     def sendMessage(channel, message, slackToken, output) {
         com.slack.api.Slack slack = com.slack.api.Slack.getInstance();
         MethodsClient methods = slack.methods(slackToken);
 
-        ChatPostMessageRequest request = ChatPostMessageRequest
-            .builder()
-            .channel(channel)
-            .text(message)
-            .build();
+        if(!this.workingDirectory.isEmpty()){
+            File tsId = new File("${this.workingDirectory}/tsId")
+            if(tsId.exists()) {
+                ChatPostMessageRequest requestNew = ChatPostMessageRequest
+                    .builder()
+                    .channel(channel)
+                    .text(message)
+                    .threadTs(tsId.text)
+                    .build();
+            
+                ChatPostMessageResponse responseNew = methods.chatPostMessage(requestNew);
+                tsId.text = responseNew.getTs()
+            } else {
+                ChatPostMessageRequest requestOld = ChatPostMessageRequest
+                    .builder()
+                    .channel(channel)
+                    .text(message)
+                    .build();
 
-        ChatPostMessageResponse response = methods.chatPostMessage(request);
+                ChatPostMessageResponse responseOld = methods.chatPostMessage(requestOld);
+                tsId.text = responseOld.getTs()
+            }
+        } else {
+            ChatPostMessageRequest requestOld = ChatPostMessageRequest
+                .builder()
+                .channel(channel)
+                .text(message)
+                .build();
 
+            ChatPostMessageResponse responseOld = methods.chatPostMessage(requestOld);
+        }
+        
         output << "Slack Messaged sent successfuly...\n"
     }
 }
